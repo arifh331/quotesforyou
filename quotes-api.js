0343793ea1d984996e88db35dd148d5e85900095
Arif Howlader <ah3288@nyu.edu>
2023-04-30 22:59:06 -0400

    first ones

---
+++ quotes-api.js
@@ -0,0 +1,61 @@
+
+const quoteEl = document.querySelector(".quote-text");
+const authorEl = document.querySelector(".quote-author");
+
+
+
+
+
+function backgroundChanger(){
+  let randomId = Math.floor(Math.random() * 10000000);
+document.querySelector(".container").style.background =
+  "url('https://source.unsplash.com/random/?Landscape&" +
+  randomId +
+  "') center/cover no-repeat";
+
+}
+
+
+
+function bodybackgroundChanger(){
+  let randomId = Math.floor(Math.random() * 10000000);
+document.querySelector("body").style.background =
+  "url('https://source.unsplash.com/random/?Landscape&" +
+  randomId +
+  "') center/cover no-repeat";
+
+}
+
+
+
+//bodybackgroundChanger()
+backgroundChanger()
+
+
+
+
+
+
+
+
+
+///Regular quotes
+
+
+async function randomQuote() {
+  let response = await fetch("https://api.quotable.io/random");
+  let data = await response.json();
+
+  quoteEl.textContent = data.content;
+  authorEl.textContent = data.author;
+
+  backgroundChanger()
+  bodybackgroundChanger()
+  
+}
+
+
+
+
+
+randomQuote();
